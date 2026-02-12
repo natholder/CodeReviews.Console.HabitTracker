48ec3d9c5835bc2ff8e9af65af466bbd35ebe437
Nat Holder <redlohtan@gmail.com>
2026-02-11 22:47:10 -0500

    got inital sqlite db set up

---
+++ HabitTracker.natholder/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Data.Sqlite;
+
+namespace HabitTracker
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string dbFileName = "HabitTracker.db";
+            string connectionString = $"Data Source ={dbFileName}";
+
+            try
+            {
+                using (var connection = new SqliteConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine($"Database {dbFileName} created or opened successfully.");
+                    CreateTable(connection);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+
+        static void CreateTable(SqliteConnection connection)
+        {
+            string CreateTableQuery = @"
+            CREATE TABLE IF NOT EXISTS Habits (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Date DATETIME,
+            Miles INT
+            )";
+
+            using (var command = new SqliteCommand(CreateTableQuery, connection))
+            {
+                command.ExecuteNonQuery();
+                Console.WriteLine("Table 'Habits' created or already exists.");
+            }
+        }
+    }
+}
